242da1119a1c0d454fa2a5f68daafad6ddbf5951
Bruno Cauet <brunocauet@gmail.com>
2014-01-04 18:43:42 +0100

    SubmissionWatcher fully implemented
    - retrieve comments
    - looks for popcorn pissers
    - writes a message

--- pp.py
+++ pp.py
@@ -4,8 +4,10 @@ This module defines a reddit bot that will look at SRD submissions, follow the
 link and look for "popcorn pissers".
 """
 from configparser import ConfigParser
+from io import StringIO
 from threading import Thread
 from time import sleep
+from collections import deque
 import logging
 
 import praw
@@ -54,10 +56,14 @@ class SubmissionWatcher(Thread):
     """Each instance is responsible for watching a single reddit thread"""
     def __init__(self, submission):
         super(SubmissionWatcher, self).__init__()
+
         self.submission = submission
+        self.target = r.get_submission(submission.url)
+
         self.popcorn_pissers = []
         self.commenters_seen = set()
-        self.comment = None
+
+        self.comment_posted = None
 
     def is_member_of_subreddit(self, user, subreddit):
         """Return whether `user` is active in `subreddit`
@@ -65,11 +71,26 @@ class SubmissionWatcher(Thread):
         That check is more tricky than it might seem: if `user` previously
         pissed in the popcorn in `subreddit` he may be seen as a member.
         """
-        return False
+        overview = user.get_overview(limit=100)
+        return any(o.subreddit == subreddit for o in overview)
 
     def get_commenters(self):
         """Get all commenters and their comments"""
-        return []
+        commenters = dict()  # author to comments dict
+        comments = deque(self.target.comments)  # comments left to treat
+        while len(comments):
+            c = comments.popleft()
+            comments.extend(c.replies)
+
+            if c.author in self.commenters_seen:
+                continue
+
+            if c.author not in commenters:
+                commenters[c.author] = []
+            commenters[c.author].append(c)
+
+        self.commenters_seen |= commenters.keys()
+        return commenters
 
     def we_can_handle_it(self):
         """Return whether we're able to watch the submission"""
@@ -85,20 +106,30 @@ class SubmissionWatcher(Thread):
 
         while True:
             for user, comments in self.get_commenters():
-                if not self.is_member_of_subreddit(user, self.target_subreddit):
+                if not self.is_member_of_subreddit(user, self.target.subreddit):
                     self.popcorn_pissers.append((user, comments))
 
             logging.info("Found %s popcorn pissers in thread %s",
-                         len(self.popcorn_pissers), self.submission)
-            self.generate_report()
+                         len(self.popcorn_pissers), self.submission.short_link)
+            if len(self.popcorn_pissers) > 0:
+                self.generate_report()
 
     def generate_report(self):
-        report = ""
-        if self.comment is None:
-            self.comment = self.submission.add_comment(report)
+        report = self.generate_report_text()
+        if self.comment_posted is None:
+            self.comment_posted = self.submission.add_comment(report)
         else:
-            self.comment.edit(report)
+            self.comment_posted.edit(report)
 
+    def generate_report_text(self):
+        s = StringIO()
+        s.write("I found the following popcorn pissers in the thread:\n\n")
+        for user, comments in self.popcorn_pissers:
+            s.write("* /u/%s: " % user.name)
+            for i, c in enumerate(comments):
+                s.write("[%s](%s) " % (i + 1, c.permalink))
+            s.write("\n")
+        return s.getvalue()
 
 if __name__ == '__main__':
     pp = PopcornPisser()
